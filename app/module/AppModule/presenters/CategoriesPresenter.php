9c2d4bf18ce5a3744d5ffb876fe90346114328ee
Petr Poupě <petr.poupe@gmail.com>
2015-06-02 10:57:21 +0200

    create demo categories

--- app/module/AppModule/presenters/CategoriesPresenter.php
+++ app/module/AppModule/presenters/CategoriesPresenter.php
@@ -16,17 +16,108 @@ class CategoriesPresenter extends BasePresenter
 	{
 		$categoryRepo = $this->em->getRepository(Category::getClassName());
 		
-//		$parent = $categoryRepo->find(5);
-//		
-//		$category = new Category('Sixth');
-//		$category->parent = $parent;
-//		$categoryRepo->save($category);
-		
-		$category = $categoryRepo->find(6);
-		$category->name = 'Sixth 6';
-		$categoryRepo->save($category);
+//		$this->createDemoCategories();
 		
 		$this->template->categories = $categoryRepo->findAll();
+	}
+	
+	private function createDemoCategories()
+	{
+		$categoryRepo = $this->em->getRepository(Category::getClassName());
+		
+		$category1 = new Category('Ladies');
+		$category1->translate('cs')->name = 'Ženy';
+		$category1->translate('sk')->name = 'Ženy';
+		$category1->mergeNewTranslations();
+		$categoryRepo->save($category1);
+		
+		$category2 = new Category('Shoes');
+		$category2->parent = $category1;
+		$category2->mergeNewTranslations();
+		$categoryRepo->save($category2);
+		
+		$category2 = new Category('Jeans');
+		$category2->parent = $category1;
+		$category2->mergeNewTranslations();
+		$categoryRepo->save($category2);
+		
+		$category2 = new Category('T-Shirts');
+		$category2->parent = $category1;
+		$category2->mergeNewTranslations();
+		$categoryRepo->save($category2);
+		
+		$category1 = new Category('Mens');
+		$category1->translate('cs')->name = 'Muži';
+		$category1->translate('sk')->name = 'Muži';
+		$category1->mergeNewTranslations();
+		$categoryRepo->save($category1);
+		
+		$category2 = new Category('Shoes');
+		$category2->parent = $category1;
+		$category2->mergeNewTranslations();
+		$categoryRepo->save($category2);
+		
+		$category3 = new Category('Classic');
+		$category3->parent = $category2;
+		$category3->mergeNewTranslations();
+		$categoryRepo->save($category3);
+		
+		$category4 = new Category('Classic 1');
+		$category4->parent = $category3;
+		$category4->mergeNewTranslations();
+		$categoryRepo->save($category4);
+		
+		$category4 = new Category('Classic 2');
+		$category4->parent = $category3;
+		$category4->mergeNewTranslations();
+		$categoryRepo->save($category4);
+		
+		$category3 = new Category('Sport');
+		$category3->parent = $category2;
+		$category3->mergeNewTranslations();
+		$categoryRepo->save($category3);
+		
+		$category4 = new Category('Sport 1');
+		$category4->parent = $category3;
+		$category4->mergeNewTranslations();
+		$categoryRepo->save($category4);
+		
+		$category4 = new Category('Sport 2');
+		$category4->parent = $category3;
+		$category4->mergeNewTranslations();
+		$categoryRepo->save($category4);
+		
+		$category2 = new Category('Trainers');
+		$category2->parent = $category1;
+		$category2->mergeNewTranslations();
+		$categoryRepo->save($category2);
+		
+		$category2 = new Category('Jeans');
+		$category2->parent = $category1;
+		$category2->mergeNewTranslations();
+		$categoryRepo->save($category2);
+		
+		$category2 = new Category('Chinos');
+		$category2->parent = $category1;
+		$category2->mergeNewTranslations();
+		$categoryRepo->save($category2);
+		
+		$category2 = new Category('T-Shirts');
+		$category2->parent = $category1;
+		$category2->mergeNewTranslations();
+		$categoryRepo->save($category2);
+		
+		$category1 = new Category('Kids');
+		$category1->mergeNewTranslations();
+		$categoryRepo->save($category1);
+		
+		$category1 = new Category('Accessories');
+		$category1->mergeNewTranslations();
+		$categoryRepo->save($category1);
+		
+		$category1 = new Category('Sports');
+		$category1->mergeNewTranslations();
+		$categoryRepo->save($category1);
 	}
 
 }
