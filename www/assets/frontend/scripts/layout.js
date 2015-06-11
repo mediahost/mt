b2329c4bdd53e5de91bdc9427a3d2b429d806f9e
Petr Poupě <petr.poupe@gmail.com>
2015-06-11 08:27:22 +0200

    change header (SEARCH)

--- www/assets/frontend/scripts/layout.js
+++ www/assets/frontend/scripts/layout.js
@@ -118,40 +118,6 @@ var Layout = function () {
         });
     };
 
-    var handleSearch = function() {    
-        $('.search-btn').click(function () {            
-            if($('.search-btn').hasClass('show-search-icon')){
-                if ($(window).width()>767) {
-                    $('.search-box').fadeOut(300);
-                } else {
-                    $('.search-box').fadeOut(0);
-                }
-                $('.search-btn').removeClass('show-search-icon');
-            } else {
-                if ($(window).width()>767) {
-                    $('.search-box').fadeIn(300);
-                } else {
-                    $('.search-box').fadeIn(0);
-                }
-                $('.search-btn').addClass('show-search-icon');
-            } 
-        }); 
-
-        // close search box on body click
-        if($('.search-btn').size() != 0) {
-            $('.search-box, .search-btn').on('click', function(e){
-                e.stopPropagation();
-            });
-
-            $('body').on('click', function() {
-                if ($('.search-btn').hasClass('show-search-icon')) {
-                    $('.search-btn').removeClass("show-search-icon");
-                    $('.search-box').fadeOut(300);
-                }
-            });
-        }
-    };
-
     var handleMenu = function() {
         $(".header .navbar-toggle").click(function () {
             if ($(".header .navbar-collapse").hasClass("open")) {
@@ -172,7 +138,7 @@ var Layout = function () {
                 }
             }
         });        
-    }
+    };
 
     var handleSidebarMenu = function () {
         $(".sidebar .dropdown > a").click(function (event) {
@@ -192,7 +158,7 @@ var Layout = function () {
     function handleDifInits() { 
         $(".header .navbar-toggle span:nth-child(2)").addClass("short-icon-bar");
         $(".header .navbar-toggle span:nth-child(4)").addClass("short-icon-bar");
-    }
+    };
 
     function handleUniform() {
         if (!jQuery().uniform) {
@@ -207,7 +173,7 @@ var Layout = function () {
                     }
                 });
         }
-    }
+    };
 
     var handleFancybox = function () {
         if (!jQuery.fancybox) {
@@ -233,7 +199,7 @@ var Layout = function () {
                 type: 'iframe'
             });
         }
-    }
+    };
 
     // Handles Bootstrap Accordions.
     var handleAccordions = function () {
@@ -242,7 +208,7 @@ var Layout = function () {
             Layout.scrollTo($(e.target), -100);
         });
         
-    }
+    };
 
     // Handles Bootstrap Tabs.
     var handleTabs = function () {
@@ -303,7 +269,6 @@ var Layout = function () {
             handleInit();
             handleResponsiveOnResize();
             handleIEFixes();
-            handleSearch();
             handleFancybox();
             handleDifInits();
             handleSidebarMenu();
