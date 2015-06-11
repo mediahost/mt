eefd5d8c9dbb0b0eb383bee253d3e4aadfd2c776
Petr Poupě <petr.poupe@gmail.com>
2015-06-11 08:27:37 +0200

    clear homepage

--- app/module/FrontModule/presenters/HomepagePresenter.php
+++ app/module/FrontModule/presenters/HomepagePresenter.php
@@ -7,17 +7,7 @@ class HomepagePresenter extends BasePresenter
 
 	public function actionDefault()
 	{
-		$this->activeCategory = $this->categoryRepo->find(10);
-	}
-
-	public function renderTest1()
-	{
-		$this->template->backlink = $this->storeRequest();
-	}
-
-	public function renderTest2()
-	{
-		$this->template->backlink = $this->storeRequest();
+		
 	}
 
 }
