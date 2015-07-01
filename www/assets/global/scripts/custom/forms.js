42eff79a0954557cee53a3be9b46ca94f148127a
Petr Poupě <petr.poupe@gmail.com>
2015-07-01 21:08:01 +0200

    refactoring

--- www/assets/global/scripts/custom/forms.js
+++ www/assets/global/scripts/custom/forms.js
@@ -1,17 +1,8 @@
 var Forms = function () {
 
-	var handleSelectSendOnChange = function () {
-		// místo:
-		// $('form select.sendOnChange').live('change', function() { ... });
-		// lze použít lepší:
-		$('body').delegate('form.sendOnChange input, form.sendOnChange select', 'change', function () {
-			$(this).closest('form').submit();
-		});
-	};
-
 	return {
 		init: function () {
-			handleSelectSendOnChange();
+
 		}
 	};
 
