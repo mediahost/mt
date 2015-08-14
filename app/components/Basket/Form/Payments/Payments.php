58bed52d3b06a3aa12329e1abe32ad01e4e9822e
Petr Poupě <petr.poupe@gmail.com>
2015-08-14 09:17:21 +0200

    Payments page demo

---
+++ app/components/Basket/Form/Payments/Payments.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Components\Basket\Form;
+
+use App\Components\BaseControl;
+use App\Forms\Form;
+use App\Forms\Renderers\MetronicFormRenderer;
+use App\Model\Facade\BasketFacade;
+
+class Payments extends BaseControl
+{
+
+	/** @var BasketFacade @inject */
+	public $basketFacade;
+
+	// <editor-fold desc="events">
+
+	/** @var array */
+	public $onAfterSave = [];
+
+	// </editor-fold>
+
+	/** @return Form */
+	protected function createComponentForm()
+	{
+		$form = new Form();
+		$form->setTranslator($this->translator);
+		$form->setRenderer(new MetronicFormRenderer());
+		if ($this->isAjax) {
+			$form->getElementPrototype()->class('ajax loadingNoOverlay');
+		}
+
+		$shippings = [
+			1 => 'Osobně',
+			2 => 'PPL',
+			3 => 'DPD',
+			4 => 'Česká Pošta',
+		];
+		$payments = [
+			1 => 'Hotově',
+			2 => 'Na dobírku',
+			3 => 'Platba předem',
+			4 => 'Kartou',
+		];
+		$form->addRadioList('shipping', 'cart.shipping', $shippings);
+		$form->addRadioList('payment', 'cart.payment', $payments);
+
+		$form->addSubmit('save', 'cart.continue');
+
+		$form->setDefaults($this->getDefaults());
+		$form->onSuccess[] = $this->formSucceeded;
+		return $form;
+	}
+
+	public function formSucceeded(Form $form, $values)
+	{
+		$this->onAfterSave();
+	}
+
+	/** @return array */
+	protected function getDefaults()
+	{
+		$values = [];
+		return $values;
+	}
+
+}
+
+interface IPaymentsFactory
+{
+
+	/** @return Payments */
+	function create();
+}
--- app/module/FrontModule/presenters/CartPresenter.php
+++ app/module/FrontModule/presenters/CartPresenter.php
@@ -2,8 +2,14 @@
 
 namespace App\FrontModule\Presenters;
 
+use App\Components\Basket\Form\IPaymentsFactory;
+use App\Components\Basket\Form\Payments;
+
 class CartPresenter extends BasePresenter
 {
+
+	/** @var IPaymentsFactory @inject */
+	public $iPaymentsFactory;
 
 	public function actionDefault()
 	{
@@ -12,7 +18,30 @@ class CartPresenter extends BasePresenter
 
 	public function actionPayments()
 	{
-		
+		$this->checkEmptyCart();
+	}
+
+	public function actionAddress()
+	{
+		$this->checkEmptyCart();
+	}
+	
+	public function checkEmptyCart()
+	{
+		if ($this->basketFacade->getIsEmpty()) {
+			$this->redirect('default');
+		}
+	}
+
+	/** @return Payments */
+	public function createComponentPayments()
+	{
+		$control = $this->iPaymentsFactory->create();
+		$control->setAjax(TRUE);
+		$control->onAfterSave = function () {
+			
+		};
+		return $control;
 	}
 
 }
