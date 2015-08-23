6f9b41022ca329c779e14a20aeb109683171a9f9
martinsifra <me@martinsifra.cz>
2015-08-23 20:57:51 +0200

    Newsletter admin init

---
+++ app/module/AppModule/presenters/SubscriberPresenter.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\AppModule\Presenters\BasePresenter;
+
+namespace App\AppModule\Presenters;
+
+class SubscriberPresenter extends BasePresenter
+{
+
+	/**
+	 * @secured
+	 * @resource('subscriber')
+	 * @privilege('default')
+	 */
+	public function actionDefault()
+	{
+
+	}
+
+	/**
+	 * @secured
+	 * @resource('subscriber')
+	 * @privilege('add')
+	 */
+	public function actionAdd()
+	{
+		
+	}
+
+	/**
+	 * @secured
+	 * @resource('subscriber')
+	 * @privilege('edit')
+	 */
+	public function actionEdit($id)
+	{
+		
+	}
+
+	/**
+	 * @secured
+	 * @resource('subscriber')
+	 * @privilege('delete')
+	 */
+	public function actionDelete($id)
+	{
+		
+	}
+
+}
