d53f81a26e5ad7149a8d111d360b42ad686116cf
Martin <Martin@ASUS-N45SF>
2015-09-15 16:38:24 +0200

    Buyout messages beforeSend()

--- app/mail/messages/Buyout/BuyoutOurMessage/OurMessage.php
+++ app/mail/messages/Buyout/BuyoutOurMessage/OurMessage.php
@@ -5,11 +5,11 @@ namespace App\Mail\Messages\Buyout;
 class OurMessage extends BaseMessage
 {
 
-	protected function build()
+	protected function beforeSend()
 	{
 		$this->addTo($this->settings->modules->buyout->email);
-		$this->setSubject('Buyout request');
-		return parent::build();
+		$this->setSubject($this->translator->translate('buyout.email.our.subject'));
+		parent::beforeSend();
 	}
 
 }
--- app/mail/messages/Buyout/BuyoutTheirMessage/TheirMessage.php
+++ app/mail/messages/Buyout/BuyoutTheirMessage/TheirMessage.php
@@ -5,11 +5,11 @@ namespace App\Mail\Messages\Buyout;
 class TheirMessage extends BaseMessage
 {
 
-	protected function build()
+	protected function beforeSend()
 	{
 		$this->setFrom($this->settings->modules->buyout->email);
-		$this->setSubject('Buyout request');
-		return parent::build();
+		$this->setSubject($this->translator->translate('buyout.email.their.subject'));
+		parent::beforeSend();
 	}
 
 }
