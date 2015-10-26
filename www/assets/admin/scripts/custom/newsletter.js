37d4fb5ec1c68121023b4be1bbe2c4d9dea5cb29
Martin Šifra <me@martinsifra.cz>
2015-10-26 14:26:52 +0100

    Fixed newsletter JS

--- www/assets/admin/scripts/custom/newsletter.js
+++ www/assets/admin/scripts/custom/newsletter.js
@@ -3,15 +3,19 @@ var Newsletter = Newsletter || {};
 
 Newsletter.init = function () {
 	var e = document.getElementById('new-newsletter-data');
-	var data = JSON.parse(e.textContent || e.innerHTML);
-	
+
+	if (e) {
+		var data = JSON.parse(e.textContent || e.innerHTML);
+
+		$(document).on('change', '#new-newsletter-locale', function () {
+			if ($('#new-newsletter-recipients').val() === 'u') {
+				$('#new-newsletter-count').text(data[$('#new-newsletter-locale').val()]);
+			}
+		});
+	}
+
+
 	$(document).on('change', '#new-newsletter-recipients', function () {
 		$('#new-newsletter-validate').click();
-	});
-	
-	$(document).on('change', '#new-newsletter-locale', function () {
-		if ($('#new-newsletter-recipients').val() === 'u') {
-			$('#new-newsletter-count').text(data[$('#new-newsletter-locale').val()]);
-		}
 	});
 };
