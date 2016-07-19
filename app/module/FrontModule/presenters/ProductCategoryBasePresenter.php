8b9c91a363133eb4a0dc07b36cac1b89f083e192
Petr Poupě <petr.poupe@gmail.com>
2016-07-19 09:57:36 +0200

    Refactoring

--- app/module/FrontModule/presenters/ProductCategoryBasePresenter.php
+++ app/module/FrontModule/presenters/ProductCategoryBasePresenter.php
@@ -2,32 +2,7 @@
 
 namespace App\FrontModule\Presenters;
 
-use App\BaseModule\Presenters\BasePresenter as BaseBasePresenter;
-use App\Components\Auth\ISignInFactory;
-use App\Components\Auth\SignIn;
-use App\Components\Newsletter\Form\ISubscribeFactory;
-use App\Components\Newsletter\Form\Subscribe;
-use App\Components\Producer\Form\IModelSelectorFactory;
-use App\Components\Producer\Form\ModelSelector;
-use App\Components\Product\Form\IPrintStockFactory;
-use App\Extensions\Products\IProductListFactory;
-use App\Extensions\Products\ProductList;
-use App\Forms\Form;
-use App\Helpers;
 use App\Model\Entity\Category;
-use App\Model\Entity\Page;
-use App\Model\Entity\Producer;
-use App\Model\Entity\ProducerModel;
-use App\Model\Entity\Product;
-use App\Model\Entity\Sign;
-use App\Model\Entity\Stock;
-use App\Model\Entity\Voucher;
-use App\Model\Facade\CategoryFacade;
-use App\Model\Repository\CategoryRepository;
-use App\Model\Repository\ProductRepository;
-use App\Model\Repository\StockRepository;
-use Nette\Application\UI\Multiplier;
-use Nette\Utils\ArrayHash;
 
 abstract class ProductCategoryBasePresenter extends BasePresenter
 {
