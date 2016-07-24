c5091f0e7bf277d4e36d6c4f0ffb2e853f3505e9
Petr Poupě <petr.poupe@gmail.com>
2016-07-24 13:09:52 +0200

    Getters for sign moved to homepage presenter

--- app/module/FrontModule/presenters/HomepagePresenter.php
+++ app/module/FrontModule/presenters/HomepagePresenter.php
@@ -11,6 +11,11 @@ class HomepagePresenter extends BasePresenter
 
 	public function renderDefault()
 	{
+		$this->template->newStocks = $this->stockFacade->getNews();
+		$this->template->saleStocks = $this->stockFacade->getSales();
+		$this->template->topStocks = $this->stockFacade->getTops();
+//		$this->template->bestsellerStocks = $this->stockFacade->getBestSellers();
+
 		$titleText = $this->translator->translate('shopTitle');
 		$this->changePageInfo(self::PAGE_INFO_TITLE, $titleText);
 	}
