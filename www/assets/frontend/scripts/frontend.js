aea9bdba5dbfb83468a8de0ca60ff9a79894672b
Petr Poupě <pupe.dupe@gmail.com>
2016-09-22 11:23:18 +0200

    Fixed bug with popup

--- www/assets/frontend/scripts/frontend.js
+++ www/assets/frontend/scripts/frontend.js
@@ -26,6 +26,22 @@ var Frontend = function () {
 				}
 			});
 		});
+
+
+		$('.popup-image').magnificPopup({
+			type: 'image'
+		});
+
+		$('.popup-text').magnificPopup({
+			removalDelay: 500,
+			closeBtnInside: true,
+			callbacks: {
+				beforeOpen: function() {
+					this.st.mainClass = this.st.el.attr('data-effect');
+				}
+			},
+			midClick: true
+		});
 	};
 
 	var handleInitICheck = function () {
