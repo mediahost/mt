1797ba431224d621ae64cf15acd7338479439b62
Petr Poupě <pupe.dupe@gmail.com>
2016-09-23 10:29:34 +0200

    Homepage products

--- app/module/FrontModule/presenters/HomepagePresenter.php
+++ app/module/FrontModule/presenters/HomepagePresenter.php
@@ -14,10 +14,11 @@ class HomepagePresenter extends BasePresenter
 		$this->stockComponentClasses[] = 'product-sm owl-item-slide';
 		$this->stockComponentLabels = FALSE;
 		$this->stockComponentSecondImage = FALSE;
+
 		$this->template->newStocks = $this->stockFacade->getNews(25);
-//		$this->template->saleStocks = $this->stockFacade->getSales();
-//		$this->template->topStocks = $this->stockFacade->getTops();
-//		$this->template->bestsellerStocks = $this->stockFacade->getBestSellers();
+		$this->template->saleStocks = $this->stockFacade->getSales(25);
+		$this->template->topStocks = $this->stockFacade->getTops(25);
+		$this->template->lastStocks = $this->user->getStorage()->getVisited(25);
 
 		$titleText = $this->translator->translate('shopTitle');
 		$this->changePageInfo(self::PAGE_INFO_TITLE, $titleText);
