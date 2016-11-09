7f0d680376f186ac3ba8677ab1d4124513d203b3
Petr Poupě <pupe.dupe@gmail.com>
2016-11-09 09:46:31 +0100

    Deny IPs

--- www/index.php
+++ www/index.php
@@ -4,11 +4,17 @@ $ips = [
 	'127.0.0.1',
 	'37.221.251.254', // SnS
 ];
+$deny = [
+	'85.248.59.106',
+];
 
 // Uncomment this line if you must temporarily take down your site for maintenance.
 //if (!in_array($_SERVER['REMOTE_ADDR'], $ips)) {
 //	require '.maintenance.php';
 //}
+if (in_array($_SERVER['REMOTE_ADDR'], $deny)) {
+	exit;
+}
 
 $container = require __DIR__ . '/../app/bootstrap.php';
 
