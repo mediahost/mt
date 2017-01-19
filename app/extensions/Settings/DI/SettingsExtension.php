bb1e7cff063da4651f641bd56315fec2560b0a83
Petr Poupě <pupe.dupe@gmail.com>
2017-01-19 09:33:46 +0100

    Remove rest of companyInfo settings

--- app/extensions/Settings/DI/SettingsExtension.php
+++ app/extensions/Settings/DI/SettingsExtension.php
@@ -218,7 +218,6 @@ class SettingsExtension extends CompilerExtension
 				->setClass('App\Extensions\Settings\SettingsStorage')
 				->addSetup('setPageInfo', [$config['pageInfo']])
 				->addSetup('setPageConfig', [$config['pageConfig']])
-				->addSetup('setCompanyInfo', [$config['companyInfo']])
 				->addSetup('setMails', [$config['mails']])
 				->addSetup('setExpiration', [$config['expiration']])
 				->addSetup('setPasswords', [$config['passwords']])
