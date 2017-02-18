a1727d9d8394dae776c448aa00fdb2a811455472
Petr Poupě <pupe.dupe@gmail.com>
2017-02-18 09:32:41 +0100

    Improved index

--- www/index.php
+++ www/index.php
@@ -1,19 +1,20 @@
 <?php
 
-$ips = [
-	'127.0.0.1',
-	'37.221.251.254', // SnS
-];
 $deny = [
 	'85.248.59.106',
 ];
-
-// Uncomment this line if you must temporarily take down your site for maintenance.
-//if (!in_array($_SERVER['REMOTE_ADDR'], $ips)) {
-//	require '.maintenance.php';
-//}
 if (in_array($_SERVER['REMOTE_ADDR'], $deny)) {
 	exit;
+}
+
+$isMaintenance = FALSE;
+$ips = [
+	'127.0.0.1',
+	'37.221.251.254', // SnS
+	'37.221.251.252', // SnS
+];
+if ($isMaintenance && !in_array($_SERVER['REMOTE_ADDR'], $ips)) {
+	require '.maintenance.php';
 }
 
 $container = require __DIR__ . '/../app/bootstrap.php';
